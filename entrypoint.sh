fe053ec223394308ede76b3b0b8fb783d6392010
rem <remi.arnal@etu.uca.fr>
2023-12-13 17:41:43 +0100

    o

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,15 @@ echo '[INFO] LAUNCH CRON IN BACKGROUND'
 service cron start
 service cron status
 
-python3 /app/testFinal.py
-
 while true; do
   if [ -f "/app/cron_exist.cron" ]; then
     echo "YES CRON WORKS"
     break;
   fi
 
-  echo "[CRON LOGS] "$(grep 'CRON' /var/log/syslog)""
-    
+  echo "CRON LOGS"
+  cat /app/cron_py.log
+     
   sleep 10
 done
 
