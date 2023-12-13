a61514ceab9006aa396bcd2452a2768a03523b47
rem <remi.arnal@etu.uca.fr>
2023-12-13 16:57:26 +0100

    start cron + status

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 echo "[INFO] STARTING AI MODEL TRAINING SERVER"
 echo "[INFO] LAUNCH CRON IN BACKGROUND"
 echo "[INFO] "USER:$USER""
-#sudo service cron start
-#sudo service cron status
+
+sudo service cron start
+sudo service cron status
